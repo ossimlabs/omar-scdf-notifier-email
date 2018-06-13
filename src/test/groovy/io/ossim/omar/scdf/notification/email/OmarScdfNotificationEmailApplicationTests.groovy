854d9f73159d299b1e6049b2b5707fc143de1040
Cale Cohee <calecohee@gmail.com>
2018-06-13 10:25:30 -0400

    Commenting out needless tests

--- src/test/groovy/io/ossim/omar/scdf/notification/email/OmarScdfNotificationEmailApplicationTests.groovy
+++ src/test/groovy/io/ossim/omar/scdf/notification/email/OmarScdfNotificationEmailApplicationTests.groovy
@@ -1,4 +1,4 @@
-package io.ossim.omar.scdf.aggregator
+/*package io.ossim.omar.scdf.aggregator
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,3 +14,4 @@ class OmarScdfNotificationEmailApplicationTests {
 	}
 
 }
+*/
